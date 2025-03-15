acca8bb8be5ac4ec9ad5b389642e2ee737c339ac
hoseinvahediedu <hoseinvahediedu@gmail.com>
2025-03-15 19:51:11 +0330

    v1-d5

---
+++ script.js
@@ -0,0 +1,19 @@
+// درس سوم هستم با علامت اسلش کامنت میذاریم
+
+var hosein = "pedar hana";
+
+console.log(hosein);
+
+var first_name="hosein";
+var last_name="vahedi";
+var age=36;
+var bio = first_name+" "+last_name+" "+age;
+console.log(bio);
+
+//---درس پنجم هستم
+// let - const  [جلوگیری از تکرار عدد و استرینگ]
+
+let f_name="hosein";
+const hoghoogh=500000;
+
+console.log (f_name+hoghoogh);
